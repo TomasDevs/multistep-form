44efaa0f1e0f838d9af32fa09c4d2dcb38835212
TomasDevs <tomas.stveracek@gmail.com>
2023-10-24 20:16:54 +0200

    Add toggle active class active for currentStep

--- src/components/formNavigation.js
+++ src/components/formNavigation.js
@@ -1,5 +1,6 @@
 import createContainer from "./createContainer";
 import createFormStep from "./createFormStep";
+import {currentStep} from '../app';
 
 // Main navigation for all steps
 const formNavigation = () => {
@@ -9,6 +10,16 @@ const formNavigation = () => {
     const stepTwo = createFormStep("2", "Step 2", "Select Plan");
     const stepThree = createFormStep("3", "Step 3", "Add-ons");
     const stepFour = createFormStep("4", "Step 4", "Summary");
+
+    if (currentStep === 1) {
+        stepOne.classList.add("active");
+    } else if (currentStep === 2) {
+        stepTwo.classList.add("active");
+    } else if (currentStep === 3) {
+        stepThree.classList.add("active");
+    } else if (currentStep === 4) {
+        stepFour.classList.add("active");
+    }
 
     containerNav.appendChild(stepOne)
     containerNav.appendChild(stepTwo)
