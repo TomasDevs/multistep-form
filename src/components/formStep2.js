34fde1914f94bf3d43ef85f16a979536fb30cd98
TomasDevs <tomas.stveracek@gmail.com>
2023-10-24 13:31:32 +0200

    Repair formstep2

--- src/components/formStep2.js
+++ src/components/formStep2.js
@@ -1,8 +1,7 @@
 import createContainer from "./createContainer"
 import createTitleHeading from "./createTitleHeading";
-import createButton from "./createButton";
 
-const formStep2 = (display) => {
+const formStep2 = () => {
     const contentContainerPlan = createContainer("container__content--plan");
 
     const heading = createTitleHeading("heading-class", "paragraph-class", "Select your plan", "You have the option of monthly or yearly billing.");
